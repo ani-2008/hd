f56bd0e10173055381667c43297b83b9c650bfb4
bitabyte <anirudan2008@gmail.com>
2026-03-18 20:38:35 +0530

    completed :3

---
+++ src/hd.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main()
+{
+    int c;
+    int mem_i = 0;
+    char *s = (char *) calloc(16, sizeof(char));
+
+    while((c = getchar()) != EOF){ 
+        if( c < 0x20 || c >= 0x7E){
+            s[mem_i % 16] = '.';
+        }else{
+            s[mem_i % 16] = c;
+        }
+        if(mem_i == 0x0){
+            printf("%.8x ",0);
+        }
+        printf("%.2x ",c);
+        mem_i++;
+        if(mem_i % 16 == 0){
+            printf("  |%s|", s);
+            printf("\n");
+            printf("%.8x ",mem_i);
+            memset(s,0,16);
+        }
+        
+    }
+    int missing = 16 - (int)strlen(s);
+
+    for(int i = 0; i < missing; i++) {
+        printf("   "); 
+    }
+
+    printf("  |%s|", s);
+    printf("\n");
+    free(s);
+}
